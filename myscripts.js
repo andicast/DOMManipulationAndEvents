7e438561a0516c2af88165d8cc8a14c76d55b84e
andicast <andi.castillom@gmail.com>
2022-08-18 19:35:02 -0400

    Events and Events using funcitons
    This involved adding three different methods on how to
    add events.

--- myscripts.js
+++ myscripts.js
@@ -35,3 +35,25 @@ container3.appendChild(divcontent);
 const pcontent =document.createElement('p');
 pcontent.textContent = 'ME TOO!';
 container3.appendChild(pcontent);
+
+//Events
+//Method 2
+const btn = document.querySelector('#btn');
+//btn.onclick = () => alert('Hello World');
+
+//Method 3
+const btn2 = document.querySelector('#btn2');
+//btn2.addEventListener('click',() => {
+//    alert("Hello World");
+//});
+
+//Event with function
+function alertFunction() {
+    alert('Yay! You did it!');
+}
+
+//Method 2
+btn.onclick = alertFunction;
+
+//Method 3
+btn2.addEventListener('click',alertFunction);
